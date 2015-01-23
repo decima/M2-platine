db1255df9cd3865bc83398e862410de34537ef6c
Henri Larget <henri@larget.fr>
2015-01-23 14:39:57 +0100

    configuration
    configuration

---
+++ src/config/settings.php
@@ -0,0 +1,12 @@
+<?php
+define("CONFIG_DB_SERVER", "localhost");
+define("CONFIG_DB_LOGIN", "jinn");
+define("CONFIG_DB_PASSWORD", "jAdNK23AHMmupzWE");
+define("CONFIG_DB_DATABASE", "jinn");
+define("CONFIG_DB_PREFIX", "jn_");
+define("CONFIG_SITE_COOKIE", "MDI1NzIyZWFhOWJiZTI1ZTBjNjE1NTZmM2MxNjI2NjI");
+define("CONFIG_SITE_TITLE", "Jinn Sample");
+define("CONFIG_SITE_ENCODE", "utf-8");
+define("CONFIG_ADMIN_LOGIN", "h.larget@gmail.com");
+define("CONFIG_ADMIN_PASSWORD", "30121991");
+?>
