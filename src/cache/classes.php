9bbc2e88fc2d30d9d98c5a8dd26ba2b38a099a61
Henri Larget <henri@larget.fr>
2015-01-25 19:11:41 +0100

    install/uninstall/enable/disable modules and caches

--- src/cache/classes.php
+++ src/cache/classes.php
@@ -1,5 +1,7 @@
 <?php
 
-// @module:helloWorld
-require_once('./modules/helloWorld/module.php');
-require_once('./modules/LightNotification/module.php');
+
+// @module:LightNotification
+require_once('./modules\LightNotification\module.php');
+// @module:HelloWorld
+require_once('./modules\helloWorld\module.php');
--- src/system/moduleManager.php
+++ src/system/moduleManager.php
@@ -35,40 +35,107 @@ class ModuleManager implements SystemModule {
         return method_invoke_all("info");
     }
 
-    public function install_module($moduleName, $path) {
+    public static function is_installed($moduleName) {
+        $r = file_get_contents("./cache/installed.txt");
+        $res = explode(",", $r);
+        return (in_array($moduleName, $res));
+    }
+
+    public static function cache_i($mname) {
+        file_put_contents("./cache/installed.txt", "," . $mname, FILE_APPEND);
+    }
+
+    public static function cache_ui($mname) {
+        $r = file_get_contents("./cache/installed.txt");
+        $res = explode(",", $r);
+        foreach ($res as $k => $v) {
+            if (strtolower($v) == strtolower($mname)) {
+                unset($res[$k]);
+            }
+        }
+        $r = implode(",", $res);
+        file_put_contents("./cache/installed.txt", $r);
+    }
+
+    public static function is_enabled($moduleName) {
+        $r = file_get_contents("./cache/enabled.txt");
+        $res = explode(",", $r);
+        return (in_array($moduleName, $res));
+    }
+
+    public static function cache_ea($mname) {
+        file_put_contents("./cache/enabled.txt", "," . $mname, FILE_APPEND);
+    }
+
+    public static function cache_da($mname) {
+        $r = file_get_contents("./cache/enabled.txt");
+        $res = explode(",", $r);
+        foreach ($res as $k => $v) {
+            if (strtolower($v) == strtolower($mname)) {
+                unset($res[$k]);
+            }
+        }
+        $r = implode(",", $res);
+        file_put_contents("./cache/enabled.txt", $r);
+    }
+
+    public static function install_module($moduleName, $path) {
         require_once($path);
-        $schema = method_invoke($moduleName, "schema");
-        try {
-            Database::schema_installer($schema);
-            $res = method_invoke($moduleName, "install");
-            return $res == null ? true : $res;
-        } catch (Exception_Database $e) {
+        if (!self::is_installed($moduleName)) {
+            $schema = method_invoke($moduleName, "schema");
+            try {
+                Database::schema_installer($schema);
+                $res = method_invoke($moduleName, "install");
+                $t = $res == null ? true : $res;
+                if ($t) {
+                    self::cache_i($moduleName);
+                }
+                return $t;
+            } catch (Exception_Database $e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static function uninstall_module($moduleName, $path) {
+        require_once($path);
+        if (!self::is_installed($moduleName) || self::is_enabled($moduleName)) {
             return false;
         }
-    }
-
-    public function uninstall_module($moduleName, $path) {
-        require_once($path);
         $schema = method_invoke($moduleName, "schema");
-        Database::schema_installer($schema);
+        Database::schema_uninstaller($schema);
         $res = method_invoke($moduleName, "uninstall");
-        return $res == null ? true : $res;
+        $t = $res == null ? true : $res;
+        if ($t) {
+            self::cache_ui($moduleName);
+        }
+        return $t;
     }
 
-    public function enable_module($moduleName, $path) {
+    public static function enable_module($moduleName, $path) {
         $exists = method_invoke($moduleName, "info");
-        if ($exists == NULL) {
+        $res = false;
+        if (!self::is_enabled($moduleName) && self::is_installed($moduleName)) {
             $rust = file_get_contents("./cache/classes.php");
             $rust .= "// @module:$moduleName\nrequire_once('$path');\n";
             file_put_contents("./cache/classes.php", $rust);
+
+            $res = method_invoke($moduleName, "enable");
         }
-        $res = method_invoke($moduleName, "enable");
-        return $res == null ? true : $res;
+        $t = $res !== false ? true : false;
+
+        if ($t) {
+            self::cache_ea($moduleName);
+        }
+        return $t;
     }
 
-    public function disable_module($moduleName) {
+    public static function disable_module($moduleName) {
         $rust = file_get_contents("./cache/classes.php");
-
+        if (!self::is_installed($moduleName) || !self::is_enabled($moduleName)) {
+            return false;
+        }
         $file = explode("//", $rust);
 
         foreach ($file as $row_id => $row) {
@@ -79,7 +146,11 @@ class ModuleManager implements SystemModule {
         $rust = implode("//", $file);
         file_put_contents("./cache/classes.php", $rust);
         $res = method_invoke($moduleName, "disable");
-        return $res == null ? true : $res;
+        $t = $res == null ? true : $res;
+        if ($t) {
+            self::cache_da($moduleName);
+        }
+        return $t;
     }
 
     public function menu($item = array()) {
@@ -89,25 +160,45 @@ class ModuleManager implements SystemModule {
         );
         $item['admin/modules/install/@'] = array(
             "access" => "administration",
-            "callback" => array("ModuleManager", "installModule")
+            "callback" => array("ModuleManager", "installModulePage")
         );
         $item['admin/modules/enable/@'] = array(
             "access" => "administration",
-            "callback" => array("ModuleManager", "enableModule")
+            "callback" => array("ModuleManager", "enableModulePage")
         );
         $item['admin/modules/disable/@'] = array(
             "access" => "administration",
-            "callback" => array("ModuleManager", "disableModule")
+            "callback" => array("ModuleManager", "disableModulePage")
         );
         $item['admin/modules/uninstall/@'] = array(
             "access" => "administration",
-            "callback" => array("ModuleManager", "uninstallModule")
+            "callback" => array("ModuleManager", "uninstallModulePage")
         );
         return $item;
     }
 
     public static function list_modules() {
+        if (isset($_GET['err'])) {
+            switch ($_GET['err']) {
+                case 'install':
 
+                    Notification::statusNotify(t("Echec de l'installation du module"), Notification::STATUS_ERROR);
+                    break;
+                case 'enable':
+                    Notification::statusNotify(t("Echec de l'activation du module"), Notification::STATUS_ERROR);
+                    break;
+                case 'disable':
+                    Notification::statusNotify(t("Echec de la desactivation du module"), Notification::STATUS_ERROR);
+                    break;
+                case 'uninstall':
+                    Notification::statusNotify(t("Echec de la désinstation du module"), Notification::STATUS_ERROR);
+                    break;
+                default:
+                    Notification::statusNotify(t("Une erreur inconnue est survenue"), Notification::STATUS_ERROR);
+
+                    break;
+            }
+        }
         $modules = self::scan_all_modules();
         $theme = new Theme();
 
@@ -116,26 +207,31 @@ class ModuleManager implements SystemModule {
         $array = array();
         foreach ($modules as $m) {
 
-            $enabled = $m["enabled"] ? $m["system_module"] ? t("système") : t("activé") : t("désactivé");
             $install = $theme->linking(Page::url("/admin/modules/install/" . $m['name']), t("installer"));
             $uninstall = $theme->linking(Page::url("/admin/modules/uninstall/" . $m['name']), t("désinstaller"));
-            $disable = t("non desactivable");
+            $disable = $theme->linking(Page::url("/admin/modules/disable/" . $m['name']), t("désactiver"));
             $enable = $theme->linking(Page::url("/admin/modules/enable/" . $m['name']), t("activer"));
 
-            if ($m['enabled']) {
-                $disable = $theme->linking(Page::url("/admin/modules/disable/" . $m['name']), t("désactiver"));
-                $enable = t("activé");
+            $statement = t("activé");
+            $link_1 = $disable;
+            $link_2 = null;
+            if (!self::is_enabled($m['name'])) {
+                $statement = t("installé");
+                $link_1 = $enable;
+                $link_2 = $uninstall;
+            }
+            if (!self::is_installed($m['name'])) {
+                $link_1=$install;$link_2 = null;
+                $statement = t("désinstallé");
             }
 
-            if ($m['system_module'] == 1) {
-                $disable = t("non-désactivable");
-                $enable = t("activé");
-                $install = t("installé");
-                $uninstall = t("non-désinstallable");
-            };
-            $rtm = ($enable . " " . $install . " " . $disable . " " . $uninstall);
 
-            $array[] = array($m["readablename"], $enabled, $rtm);
+            if ($m["system_module"] == 1) {
+                $rtm = "système";
+            } else {
+                $rtm = ($link_1).($link_2==null?"":"- ").$link_2;
+            }
+            $array[] = array($m["readablename"], $statement, $rtm);
         }
 
 
@@ -148,14 +244,63 @@ class ModuleManager implements SystemModule {
         return;
     }
 
-    public static function installModule($moduleName) {
+    public static function installModulePage($moduleName) {
 
         $modules = self::scan_all_modules();
         if (isset($modules[$moduleName])) {
-            
+            if (self::install_module($modules[$moduleName]["name"], $modules[$moduleName]["path"])) {
+                header("location: " . Page::url("/admin/modules/"));
+            } else {
+                header("location: " . Page::url("/admin/modules/?err=install"));
+            }
+        } else {
+            header("location: " . Page::url("/admin/modules/?err=unknown"));
         }
-        echo Page::url("/");
-        header("location: " . Page::url("/admin/modules/"));
+        return;
+    }
+
+    public static function enableModulePage($moduleName) {
+
+        $modules = self::scan_all_modules();
+        if (isset($modules[$moduleName])) {
+            if (self::enable_module($modules[$moduleName]["name"], $modules[$moduleName]["path"])) {
+                header("location: " . Page::url("/admin/modules/"));
+            } else {
+                header("location: " . Page::url("/admin/modules/?err=enable"));
+            }
+        } else {
+            header("location: " . Page::url("/admin/modules/?err=unknown"));
+        }
+        return;
+    }
+
+    public static function disableModulePage($moduleName) {
+
+        $modules = self::scan_all_modules();
+        if (isset($modules[$moduleName])) {
+            if (self::disable_module($modules[$moduleName]["name"], $modules[$moduleName]["path"])) {
+                header("location: " . Page::url("/admin/modules/"));
+            } else {
+                header("location: " . Page::url("/admin/modules/?err=disable"));
+            }
+        } else {
+            header("location: " . Page::url("/admin/modules/?err=unknown"));
+        }
+        return;
+    }
+
+    public static function uninstallModulePage($moduleName) {
+
+        $modules = self::scan_all_modules();
+        if (isset($modules[$moduleName])) {
+            if (self::uninstall_module($modules[$moduleName]["name"], $modules[$moduleName]["path"])) {
+                header("location: " . Page::url("/admin/modules/"));
+            } else {
+                header("location: " . Page::url("/admin/modules/?err=uninstall"));
+            }
+        } else {
+            header("location: " . Page::url("/admin/modules/?err=unknown"));
+        }
         return;
     }
 
