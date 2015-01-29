65f65dced54d21805479a5e29b2799b10f367350
Florent PEYSSON <florent.peysson@gmail.com>
2015-01-29 09:02:48 +0100

    Archivage local de jQuery et des divers css pour les polices et font-awesome.

--- src/themes/default/templates/blank.php
+++ src/themes/default/templates/blank.php
@@ -5,10 +5,10 @@
     <title><?php echo CONFIG_SITE_TITLE;?></title>
     <!-- CSS -->
     <link type="text/css" rel="stylesheet" href="<?php echo Page::url("/themes/default/styles/body.css"); ?>">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600,700,400' rel='stylesheet' type='text/css'>
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='<?php echo Page::url("/themes/default/styles/font-opensans.css"); ?>' rel='stylesheet' type='text/css'>
+    <link href="<?php echo Page::url("/themes/default/styles/font-awesome.min.css"); ?>" rel='stylesheet' type='text/css'>
     <!-- Scripts -->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript" src="<?php echo Page::url("/themes/default/scripts/jquery-1.11.2.min.js"); ?>"></script>
     <script type="text/javascript" src="<?php echo Page::url("/themes/default/scripts/jquery.elastic.source.js"); ?>"></script>
     <script type="text/javascript" src="<?php echo Page::url("/themes/default/scripts/initTheme_blank.js"); ?>"></script>
     <!-- Meta/Communs à l'appli -->
