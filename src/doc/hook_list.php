e6692d2e26ce7c79e7fd79a8422ada7b513e7ed1
Henri Larget <henri@larget.fr>
2015-03-07 13:55:00 +0100

    doc - hook-list

--- src/doc/hook_list.php
+++ src/doc/hook_list.php
@@ -3,6 +3,36 @@
 interface all_hooks_declared {
 
     /**
+     * method called on module installation
+     * @return TRUE to complete the installation, FALSE to abort the installation
+     */
+    public function install();
+
+    /**
+     * method called on module activation
+     * @return TRUE to complete the activation, FALSE to abort the activation
+     */
+    public function enable();
+
+    /**
+     * method called on module desactivation
+     * @return TRUE to complete the desactivation, FALSE to abort the desactivation
+     */
+    public function disable();
+
+    /**
+     * method called on module uninstallation
+     * @return TRUE to complete the uninstallation, FALSE to abort the uninstallation
+     */
+    public function uninstall();
+
+    /**
+     * declare the scheme of the tables in the database
+     * @return array containing the scheme of the different tables of the database
+     */
+    public function schema();
+
+    /**
      * on module installation
      * @param String $module_name the name of the module
      */
