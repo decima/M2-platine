6aeb0fe1c26f4ff1f7b654bd2651c26162f292c2
Henri Larget <henri@larget.fr>
2015-03-08 22:50:00 +0100

    mon cache

--- src/cache/classes.php
+++ src/cache/classes.php
@@ -1,7 +1,16 @@
 <?php
 
-
-// @module:HelloWorld
-require_once('./modules\helloWorld\module.php');
 // @module:User
 require_once('./modules\User\module.php');
+// @module:Group
+require_once('./modules\Group\module.php');
+// @module:Friends
+require_once('./modules\Friends\module.php');
+// @module:PermissionsManager
+require_once('./modules\PermissionsManager\module.php');
+// @module:MenuWidget
+require_once('./modules\MenuWidget\module.php');
+// @module:VGroups
+require_once('./modules\VGroups\module.php');
+// @module:Publication
+require_once('./modules\Publication\module.php');
