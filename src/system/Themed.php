95da73cb1037b8406974ce9a750a6d1595a65eee
Henri Larget <henri@larget.fr>
2015-03-08 16:40:36 +0100

    ajout js de l'objet jn

--- src/system/Themed.php
+++ src/system/Themed.php
@@ -5,7 +5,6 @@ abstract class Themed {
     private static $head = array();
     private static $menu = array();
     private static $body = array();
-
     static $title = null;
 
     const STRUCT_DEFAULT = "default";
@@ -13,9 +12,15 @@ abstract class Themed {
     const STRUCT_ADMIN = "admin";
 
     public abstract function process_theme($structure = self::STRUCT_DEFAULT);
+
     public abstract function process_404();
+
     public abstract function process_403();
-    
+
+    public function __construct() {
+        self::add_to_head("<script>var jn = { url:function(path){return '" . Page::url("") . "'+path}}</script>");
+    }
+
     public static function head() {
         foreach (self::$head as $s) {
             echo $s . "\n";
@@ -25,17 +30,21 @@ abstract class Themed {
     public static function get_head() {
         return self::$head;
     }
+
     public static function &get_menu() {
         return self::$menu;
     }
+
     public static function &get_body() {
         return self::$body;
     }
+
     public static function get_title() {
         return self::$title;
     }
-    public static function set_title($title){
-        self::$title =  $title;
+
+    public static function set_title($title) {
+        self::$title = $title;
     }
 
     public static function menu() {
@@ -43,6 +52,7 @@ abstract class Themed {
             echo $m . "\n";
         }
     }
+
     public static function add_to_menu($element) {
         self::$menu[] = $element;
     }
@@ -51,19 +61,17 @@ abstract class Themed {
         $b = self::get_body();
         $s = sizeof($b);
 
-        if(isset($index_debut)){
-            if($nb != 0){
-                $max = $index_debut+$nb-1;
-            }
-            else {
+        if (isset($index_debut)) {
+            if ($nb != 0) {
+                $max = $index_debut + $nb - 1;
+            } else {
                 $max = $s - $index_debut;
             }
 
-            for ($index_debut ; $index_debut <= $max ; $index_debut++) {
+            for ($index_debut; $index_debut <= $max; $index_debut++) {
                 echo $b[$index_debut] . "\n";
             }
-        }
-        else {
+        } else {
             foreach (self::$body as $s) {
                 echo $s . "\n";
             }
@@ -72,15 +80,15 @@ abstract class Themed {
 
     public static function add_to_body($element, $title = null, $index = 0) {
         $output = "";
-        if($title != null){
-            $output = "<h1>".$title."</h1>";
+        if ($title != null) {
+            $output = "<h1>" . $title . "</h1>";
         }
         $output .= $element;
         self::$body[] = $output;
     }
 
     public static function clean_body_part($index = 0) {
-        if(isset($index) && sizeof(self::$body) > 0)
+        if (isset($index) && sizeof(self::$body) > 0)
             self::$body[$index] = "";
     }
 
@@ -88,15 +96,15 @@ abstract class Themed {
         self::$head[] = $element;
     }
 
-    public static function linking($link, $value, $out = false, $attr = array()){
-        $output = "<a href='".$link."'";
-        if($out)
+    public static function linking($link, $value, $out = false, $attr = array()) {
+        $output = "<a href='" . $link . "'";
+        if ($out)
             $attr["target"] = "_blank";
 
-        foreach ($attr as $k => $v){
+        foreach ($attr as $k => $v) {
             $output .= " $k='$v'";
         }
-        $output .= ">".$value."</a>";
+        $output .= ">" . $value . "</a>";
         return $output;
     }
 
@@ -121,8 +129,8 @@ abstract class Themed {
         foreach ($rows as $k => $r) {
             $output .="<tr>\n";
             $a = array_shift($hcol);
-            if($a != null) {
-                $output .= "<th>".$a."</th>\n";
+            if ($a != null) {
+                $output .= "<th>" . $a . "</th>\n";
             }
             foreach ($r as $r2)
                 $output.="<td>$r2</td>\n";
@@ -134,76 +142,72 @@ abstract class Themed {
         return $output;
     }
 
-
-    public static function process_form(Form $form){
+    public static function process_form(Form $form) {
         $output = self::forming($form);
         self::add_to_body($output);
     }
 
-    public static function forming(Form $form){
+    public static function forming(Form $form) {
         $output = "";
-        $output .= "<".$form -> getBalise();
-        foreach($form->getAttributes() as $k=>$v){
+        $output .= "<" . $form->getBalise();
+        foreach ($form->getAttributes() as $k => $v) {
             $output .= " $k=\"$v\"";
         }
         $output .=">";
-        foreach($form->getElements() as $v){
+        foreach ($form->getElements() as $v) {
             $output .= static::process_form_elements($v);
         }
-        $output .="</".$form -> getBalise().">";
+        $output .="</" . $form->getBalise() . ">";
         return $output;
     }
 
-    public static function process_form_elements(FormElement $element){
+    public static function process_form_elements(FormElement $element) {
         ($element->getLabel() != null AND $element->getLabel() != "") ? $isLabel = true : $isLabel = false;
         ($element->getId() != null AND $element->getId() != "") ? $label = $element->getId() : $label = $element->getName();
         $output = "";
 
         // Element auto-fermable
-        if($element -> is_closed()){
-            if ($isLabel AND ($element->getAttributes()['type'] != 'radio' AND $element->getAttributes()['type'] != 'checkbox')) {
-                $output .= "<label for=\"".$label."\">".$element->getLabel()."</label>";
+        if ($element->is_closed()) {
+            if ($isLabel AND ( $element->getAttributes()['type'] != 'radio' AND $element->getAttributes()['type'] != 'checkbox')) {
+                $output .= "<label for=\"" . $label . "\">" . $element->getLabel() . "</label>";
             }
             $output .= "<" . $element->getBalise();
-            if($element->getId() != null AND $element->getId() != "")
+            if ($element->getId() != null AND $element->getId() != "")
                 $output .= " id=\"" . $element->getId() . "\"";
-            if($element->getName() != null AND $element->getName() != "")
+            if ($element->getName() != null AND $element->getName() != "")
                 $output .= " name=\"" . $element->getName() . "\"";
             $output .= " value=\"" . $element->getValue() . "\"";
             foreach ($element->getAttributes() as $k => $v) {
                 $output .= " $k=\"$v\"";
             }
             $output .= "/>";
-            if ($isLabel AND ($element->getAttributes()['type'] == 'radio' OR $element->getAttributes()['type'] == 'checkbox')) {
-                $output .= "<label for=\"".$label."\">" . $element->getLabel() . "</label>";
+            if ($isLabel AND ( $element->getAttributes()['type'] == 'radio' OR $element->getAttributes()['type'] == 'checkbox')) {
+                $output .= "<label for=\"" . $label . "\">" . $element->getLabel() . "</label>";
             }
-        }
-        else {
-            if($isLabel AND (strtolower($element -> getBalise()) == "select" OR strtolower($element -> getBalise()) == "textarea")){
-                $output .= "<label for=\"".$label."\">".$element -> getLabel()."</label>";
+        } else {
+            if ($isLabel AND ( strtolower($element->getBalise()) == "select" OR strtolower($element->getBalise()) == "textarea")) {
+                $output .= "<label for=\"" . $label . "\">" . $element->getLabel() . "</label>";
             }
-            $output .= "<".$element -> getBalise();
-            if($element->getId() != null AND $element->getId() != "")
+            $output .= "<" . $element->getBalise();
+            if ($element->getId() != null AND $element->getId() != "")
                 $output .= " id=\"" . $element->getId() . "\"";
-            if($element->getName() != null AND $element->getName() != "")
+            if ($element->getName() != null AND $element->getName() != "")
                 $output .= " name=\"" . $element->getName() . "\"";
-            $output .= " value=\"".$element -> getValue()."\"";
-            foreach($element->getAttributes() as $k=>$v){
+            $output .= " value=\"" . $element->getValue() . "\"";
+            foreach ($element->getAttributes() as $k => $v) {
                 $output .= " $k=\"$v\"";
             }
             $output .=">";
-            if(sizeof($element->getElements()) > 0){
-                foreach($element->getElements() as $v){
+            if (sizeof($element->getElements()) > 0) {
+                foreach ($element->getElements() as $v) {
                     $output .= self::process_form_elements($v);
                 }
-            }
-            else if(strtolower($element -> getBalise()) != "textarea") {
+            } else if (strtolower($element->getBalise()) != "textarea") {
                 $output .= $element->getLabel();
-            }
-            else {
+            } else {
                 $output .= $element->getValue();
             }
-            $output .="</".$element -> getBalise().">";
+            $output .="</" . $element->getBalise() . ">";
         }
         return $output;
     }
@@ -225,7 +229,7 @@ class View implements SystemModule {
     }
 
     public function system_init() {
-
+        
     }
 
 }
