caf8ca232b7b423662c466290b4c8632396ec986
Henri Larget <henri@larget.fr>
2015-03-12 15:17:28 +0100

    chat js

--- src/modules/Messages/chat.js
+++ src/modules/Messages/chat.js
@@ -2,29 +2,11 @@ String.prototype.endsWith = function (suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 $(document).ready(function () {
+    refreshMessages();
+    theme_init();
     setInterval(function () {
-        $.get(window.location.href + "/ajax/", function (data) {
-            $(".messagerie").remove();
-            for (var index = data.length - 1; index >= 0; index--) {
-                var icon = '<i class="fa fa-4x fa-user fa-inverse"></i>';
-                if (window.location.href.endsWith(data[index].rid)) {
-                    icon = '<i class="fa fa-4x fa-user"></i>';
-                }
-
-                var mt = data[index];
-                console.log(mt);
-                var m = '<div class="messagerie"><div class="messagerie_avatar_area">\n\
-<div class="messagerie_avatar">\n\
-    ' + icon + '\n\
-</div><div class="messagerie_nom"><a>' + mt.sender_firstname + '<br>' + mt.sender_lastname + '</a></div> \n\
-</div>\n\
-<div class="messagerie_bloc_message messagerie_bloc_message_decalageG">\n\
-<div class="messagerie_bloc_message_date">\n\
-</div> <div class="messagerie_bloc_texte_large">' + mt.message + '</div></div><div class="clear"></div></div>';
-                $("#messaging").before(m);
-            }
-        });
-
+        refreshMessages();
+        theme_init();
 
     }, 2000);
 
@@ -48,5 +30,28 @@ $(document).ready(function () {
 
 function send_message(e) {
     $.post(window.location.href + "/ajax/send", {message: e}, function (data) {
+    });
+}
+function refreshMessages() {
+    $.get(window.location.href + "/ajax/", function (data) {
+        $(".messagerie").remove();
+        for (var index = data.length - 1; index >= 0; index--) {
+            var icon = '<i class="fa fa-4x fa-user fa-inverse"></i>';
+            if (window.location.href.endsWith(data[index].rid)) {
+                icon = '<i class="fa fa-4x fa-user"></i>';
+            }
+
+            var mt = data[index];
+            console.log(mt);
+            var m = '<div class="messagerie"><div class="messagerie_avatar_area">\n\
+<div class="messagerie_avatar">\n\
+    ' + icon + '\n\
+</div><div class="messagerie_nom"><a>' + mt.sender_firstname + '<br>' + mt.sender_lastname + '</a></div> \n\
+</div>\n\
+<div class="messagerie_bloc_message messagerie_bloc_message_decalageG">\n\
+<div class="messagerie_bloc_message_date">\n\
+</div> <div class="messagerie_bloc_texte_large">' + mt.message + '</div></div><div class="clear"></div></div>';
+            $("#messaging").before(m);
+        }
     });
 }
